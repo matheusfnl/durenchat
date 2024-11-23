87376d26c461891e8000c14a30bc4213ba563c3f
matheusfnl <github@matheusfunil.dev>
2024-11-23 14:02:13 -0300

    Revalida o .gitignore

--- dist/types/classes/message.d.ts
+++
@@ -1,9 +0,0 @@
-import { MessageConstructor, MessageContent } from '../utills/types';
-import { User } from './user';
-export declare class Message {
-    sender: User;
-    content: MessageContent;
-    sent_at: Date;
-    edited_at?: Date | null;
-    constructor({ sender, content, sent_at, edited_at }: MessageConstructor);
-}
