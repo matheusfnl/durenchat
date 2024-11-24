58ec3c753e855b0a2b6c874f24243a5b2aad9209
matheusfnl <github@matheusfunil.dev>
2024-11-24 15:19:55 -0300

    hide files

--- index.js
+++ index.js
@@ -36,29 +36,22 @@ const loadIcons = async () => {
 };
 
 loadIcons().then(icons => {
-  const popupChat = Durenchat.popup({
-    container: 'body',
-    color: '#5cc1ff',
-    id: 'popup-container',
-    icons: {
-      popupOpen: icons.popupOpen,
-      popupClose: icons.popupClose,
-    },
-  });
+  // CHAT 1
 
   const chat = Durenchat.durenchat({
+    prefix: 'chat-1',
     users: [
       {
         id: 2,
         name: 'Matheus',
-        color: '#5cc1ff',
+        color: '#4a3d1f',
         textColor: '#fff',
         photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
       },
       {
         id: 1,
         name: 'Fernanda',
-        color: '#ebe6e6',
+        color: '#f7f2e4',
         textColor: '#000',
         photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
       }
@@ -79,8 +72,6 @@ loadIcons().then(icons => {
 
   const user = chat.getUser(2);
 
-  popupChat.defineChat(chat);
-
   chat.defineHeader({
     photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
     name: user.name,
@@ -89,64 +80,187 @@ loadIcons().then(icons => {
   chat.defineChatcontainer('dc-messages');
   chat.defineFooter();
 
-  chat.render('durenchat');
-
+  chat.render('chat-1');
   chat.sendMessage({
-    sender: 2,
+    sender: 1,
     content: 'Olá',
-    status: 'delivered',
   });
 
   chat.sendMessage({
     sender: 2,
-    content: { type: 'image', url: 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg', caption: 'Isso é o caption' },
-    status: 'delivered',
-  });
-
-  chat.sendMessage({
-    sender: 2,
-    content: { type: 'document', url: 'https://example.com/document.pdf', name: 'Documento', caption: 'Isso é o caption' },
-    sent_at: new Date('2021-08-10T15:00:00'),
-    status: 'delivered',
-  });
-
-  chat.sendMessage({
-    sender: 2,
-    content: { type: 'audio', url: 'https://example.com/audio.mp3' },
+    content: { type: 'image', url: 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg', caption: 'Olá, tudo bem?' },
     status: 'read',
   });
 
-  chat.sendMessage({
+  // CHAT 2
+
+  const chat2 = Durenchat.durenchat({
+    prefix: 'chat-2',
+    users: [
+      {
+        id: 2,
+        name: 'Matheus',
+        color: '#303545',
+        textColor: '#fff',
+        photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
+      },
+    ],
+    self: 2,
+    icons: {
+      sent: icons.sent,
+      delivered: icons.delivered,
+      read: icons.read,
+      options: icons.options,
+      cancelEdit: icons.cancelEdit,
+      send: icons.send,
+      emoji: icons.emoji,
+      file: icons.file,
+      microphone: icons.microphone,
+    },
+  });
+
+  chat2.defineHeader({
+    photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
+    name: user.name,
+  });
+
+  chat2.defineChatcontainer('dc-messages');
+
+  chat2.defineFooter('<div>HTML Personalizado para ser aplicado</div>');
+
+  chat2.render('chat-2');
+  chat2.sendMessage({
+    sender: 1,
+    content: 'Olá',
+  });
+
+  chat2.sendMessage({
     sender: 2,
-    content: { type: 'video', url: 'https://example.com/video.mp4', caption: 'Isso é o caption' },
+    content: { type: 'image', url: 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg', caption: 'Olá, tudo bem?' },
     status: 'read',
   });
 
-  chat.sendMessage({
-    sender: 1,
-    content: 'Texto simples',
+  // CHAT 3
+
+  const chat3 = Durenchat.durenchat({
+    prefix: 'chat-3',
+    users: [
+      {
+        id: 2,
+        name: 'Matheus',
+        color: '#19223d',
+        textColor: '#fff',
+        photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
+      },
+      {
+        id: 1,
+        name: 'Fernanda',
+        color: '#ffffff',
+        textColor: '#000',
+        photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
+      }
+    ],
+    self: 2,
+    icons: {
+      sent: icons.sent,
+      delivered: icons.delivered,
+      read: icons.read,
+      options: icons.options,
+      cancelEdit: icons.cancelEdit,
+      send: icons.send,
+      emoji: icons.emoji,
+      file: icons.file,
+      microphone: icons.microphone,
+    },
   });
 
-  chat.sendMessage({
-    sender: 1,
-    content: { type: 'image', url: 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg', caption: 'Isso é o caption' },
+  chat3.defineHeader({
+    photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
+    name: user.name,
   });
 
-  chat.sendMessage({
+  chat3.defineChatcontainer('dc-messages',
+    `<div class="chat-3-chat-container">
+      <div class="protected-messages">
+        As mensagens desse chat são protegidas
+      </div>
+    </div>`
+  );
+
+  chat3.defineFooter();
+
+  chat3.render('chat-3');
+  chat3.sendMessage({
     sender: 1,
-    content: { type: 'document', url: 'https://example.com/document.pdf', name: 'Documento', caption: 'Isso é o caption' },
+    content: 'Olá',
   });
 
-  chat.sendMessage({
-    sender: 1,
-    content: { type: 'audio', url: 'https://example.com/audio.mp3' },
-    sent_at: new Date('2021-08-10T15:00:00'),
+  chat3.sendMessage({
+    sender: 2,
+    content: { type: 'image', url: 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg', caption: 'Olá, tudo bem?' },
+    status: 'read',
   });
 
-  const message = chat.sendMessage({
-    sender: 1,
-    content: { type: 'video', url: 'https://example.com/video.mp4', caption: 'Isso é o caption' },
-  });
+    // Pop up
+    const popupChat = Durenchat.popup({
+      container: 'body',
+      color: '#5cc1ff',
+      id: 'popup-container',
+      icons: {
+        popupOpen: icons.popupOpen,
+        popupClose: icons.popupClose,
+      },
+    });
 
-  message.setStatus('delivered');
+    const chat4 = Durenchat.durenchat({
+      users: [
+        {
+          id: 2,
+          name: 'Matheus',
+          color: '#5cc1ff',
+          textColor: '#fff',
+          photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
+        },
+        {
+          id: 1,
+          name: 'Fernanda',
+          color: '#ebe6e6',
+          textColor: '#000',
+          photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
+        }
+      ],
+      self: 2,
+      icons: {
+        sent: icons.sent,
+        delivered: icons.delivered,
+        read: icons.read,
+        options: icons.options,
+        cancelEdit: icons.cancelEdit,
+        send: icons.send,
+        emoji: icons.emoji,
+        file: icons.file,
+        microphone: icons.microphone,
+      },
+    });
+
+    popupChat.defineChat(chat4);
+
+    chat4.defineHeader({
+      photoUrl: 'https://s2-techtudo.glbimg.com/L9wb1xt7tjjL-Ocvos-Ju0tVmfc=/0x0:1200x800/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/q/l/TIdfl2SA6J16XZAy56Mw/canvaai.png',
+      name: user.name,
+    });
+
+    chat4.defineChatcontainer('dc-messages');
+    chat4.defineFooter();
+
+    chat4.sendMessage({
+      sender: 1,
+      content: 'Olá',
+    });
+
+    chat4.sendMessage({
+      sender: 2,
+      content: { type: 'image', url: 'https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/1015f/MainBefore.jpg', caption: 'Olá, tudo bem?' },
+      status: 'read',
+    });
 });
